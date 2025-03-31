cbe1b5b95cf068a5365af7a1155337ce7e9f75b3
Lvidaldev <lidiavidal0725@gmail.com>
2025-03-31 11:39:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+const rememberforgotLink = document.querySelector('.rememberforgot-link');
+
 
 const btnPopup = document.querySelector('.btnLogin-popup');
 
@@ -9,10 +11,17 @@ const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
+    wrapper.classList.remove('active-remember');
 });
 
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
+rememberforgotLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active'); // remove registro, por exemplo
+    wrapper.classList.add('active-remember');
+});
+
+loginLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    wrapper.classList.remove('active', 'active-remember');
 });
 
 btnPopup.addEventListener('click', ()=> {
